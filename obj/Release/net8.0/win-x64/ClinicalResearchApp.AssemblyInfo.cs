a9ac6823cbfd61bc8ac4dcb30d708173a1de1ab8
bsharm12@jh.edu <bsharm12@jh.edu>
2025-01-29 16:37:06 -0500

    Data table update

--- obj/Release/net8.0/win-x64/ClinicalResearchApp.AssemblyInfo.cs
+++ obj/Release/net8.0/win-x64/ClinicalResearchApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClinicalResearchApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64bb678cc71fa1b95d4f5d4799bd6d2e40d35a0f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b8b5d2d079474eda70aef7098c46b27aeb29f28")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClinicalResearchApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClinicalResearchApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
