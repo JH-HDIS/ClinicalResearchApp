8f9ebc38dd02fe289319c76b8aa29e0be3c3e73f
bsharm12@jh.edu <bsharm12@jh.edu>
2025-03-14 16:02:57 -0400

    1.C.3 text update only

--- obj/Release/net8.0/win-x64/ClinicalResearchApp.AssemblyInfo.cs
+++ obj/Release/net8.0/win-x64/ClinicalResearchApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClinicalResearchApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63c3088c9e6c06e9ee752ddf25da703826ec3bdd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b240611aa82461cd74bbd9cc834df004e0f0bff3")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClinicalResearchApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClinicalResearchApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
